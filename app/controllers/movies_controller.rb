1751c69ddd7980fe82b1e9b61c9e0cc56bdd873b
ggross523 <ggross0@chicagobooth.edu>
2025-05-05 17:08:53 +0000

    delete movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,5 @@
 class MoviesController < ApplicationController
+
   def create
     m = Movie.new
     m.title = params.fetch("the_title")
@@ -13,7 +14,18 @@ class MoviesController < ApplicationController
     redirect_to("/movies")
 
   end
-  
+
+  def destroy
+    the_id = params.fetch("an_id")
+
+    matching_records = Movie.where ({ :id => the_id })
+
+    the_movie = matching_records.at(0)
+
+    the_movie.destroy
+
+    redirect_to("/movies")
+  end
   def index
     matching_movies = Movie.all
     @list_of_movies = matching_movies.order({ :created_at => :desc })
